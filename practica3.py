6e3f53b71cc2e74218c02800d519f833a69a35d0
CISCO <enriquepuga@hotmail.com>
2016-12-05 21:40:25 -0500

    prueba

---
+++ practica3.py
@@ -0,0 +1,35 @@
+a = 5
+print (a)
+print (type(a))
+a = "cinco"
+print (a)
+print (type(a))
+
+a = u"test ß "
+
+print("operacion a nivel de bits")
+print (3 & 1)
+print (3 & 2)
+print (3 << 1)
+
+a = 10
+a = a + 9
+a+=9
+print (a)
+
+
+nombre1 = "Enrique"
+nombre2 = "Enrique"
+print (nombre1 and nombre2)
+
+nacimiento = 1986
+print("Mayor de edad")
+print ((82016 - nacimiento) >= 18)
+
+edad = 15
+print ("adolecente ??  12 - 18   )")
+print  ( edad >= 12 and edad <= 18)
+print (12 <= edad <= 18)
+print ("edad no laboral ??")
+print ( 1<= edad <= 18 or 80 <= edad <= 120)
+
---
+++ proyecto2.py
@@ -0,0 +1,25 @@
+print("   Tipos de Datos     " )
+print(type (10))
+print(type (5.5))
+print(type (1+5j))
+
+print(2 +4)
+print(8**3)
+
+print("estoes una cadena")
+print('esta es 1 cadena')
+print("""
+linea1
+linea2
+linea3
+""")
+print (type("esto es una cadena"))
+
+print("cadena 1 " + "cadena 2")
+print("Enrique " + "puga ")
+print("cadena 1 " * 10)
+
+print(type(True))
+print(type(False))
+a = 5
+print (a)
